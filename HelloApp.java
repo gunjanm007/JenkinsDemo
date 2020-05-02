ca72004fc752dcf01cf7f962a55faa436d7bd766
gunjan mundra <gunjan.mundra@atos.net>
2020-05-02 19:27:48 +0530

    jenkins demo commited

--- HelloApp.java
+++ HelloApp.java
@@ -3,7 +3,7 @@ package demo1;
 public class HelloApp {
 
 	public static void main(String[] args) {
-		System.out.println("Hello gunjan welcome to Java Demo");
+		System.out.println("Hello gunjan to change");
 
 	}
 
